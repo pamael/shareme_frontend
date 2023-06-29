5f7bc58b9fbacb9a8059a189e3ea02322c02781e
Patrice Elias <patrice.elias@gmail.com>
2023-06-29 15:42:59 +0000

    Fixed home page UI

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -5,47 +5,57 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 import { Sidebar, UserProfile } from '../components';
 import Pins from './Pins';
-import { userQuery } from '../utils/data';
+//import { userQuery } from '../utils/data';
 import { client } from '../lib/client';
 import logo from '../assets/logo.png';
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
-
+  //const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
   useEffect(() => {
-    const query = userQuery(userInfo?.googleId);
+    //const query = userQuery(userInfo?.googleId);
 
-    client.fetch(query)
-      .then((data) => {
-        setUser(data[0]);
-      })
+    setUser({name: "John Doe", _id: "acfsffsfdsd", googleId:  "12345678", image: "https://is1-ssl.mzstatic.com/image/thumb/Purple112/v4/31/17/79/311779d6-bfe8-d8d5-4782-81bd4c5f01ea/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.webp"});
+    // client.fetch(query)
+    //   .then((data) => {
+    //     setUser(data[0]);
+    //   })
   }, []);
 
-  return (
-    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-      <div className='hidden md:flex h-screen flex-initial '>
+  return (    
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
         <Sidebar user={user && user} />
       </div>
-      <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(false)}/>
-        <Link to="/">
-          <img src={logo} alt="logo" className='w-28'/>
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logo" className='w-28'/>
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
-          <div className='absolute w-full flex justify-end items-center p-2'>
-            <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={() => setToggleSidebar(true)}/>
-          </div>
-          <Sidebar user={user && user} />
+      <div className="flex md:hidden flex-row">
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)}/>
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28"/>
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logo" className="w-28"/>
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+            </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+      {/* <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div> */}
     </div>
   )
 }
